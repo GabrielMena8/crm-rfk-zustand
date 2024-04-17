f9830d3be12bf8e0e1e216caa6239551c57d3158
GabrielMena8 <114101881+GabrielMena8@users.noreply.github.com>
2024-04-17 11:52:03 -0400

    Listo los primeros componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import FormPacientes from "./components/FormPacientes"
+import ListPacientes from "./components/ListPacientes"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+
+      <div className="container  mx-auto mt-20 " >
+        <h1 className="text-4xl font-black text-purple-800 uppercase md:w-2/3 md:mx-auto outline-dashed py-2 text-center outline-purple-400">  
+          Seguimiento {''}
+          <span className="">de</span> {''}
+          <span className="text-indigo-500">Pacientes</span>
+        </h1>
+
+
+
+        <div className="md:flex mt-12">
+          <FormPacientes />
+
+          <ListPacientes />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+     
     </>
   )
 }
---
+++ src/components/ListPacientes.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ListPaciente() {
+  return (
+    <div>ListPaciente</div>
+  )
+}
